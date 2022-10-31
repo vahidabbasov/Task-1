878ac87193e680e4ad7a98ab0da20179b4dcff05
vahidabbasov <tu7hl7kxu@code.edu.az>
2022-11-01 01:48:05 +0400

    Created c# & js files

---
+++ Tasks c#/Task01/Program.cs
@@ -0,0 +1,57 @@
+﻿#region Task 1. Verilmiş ədədin 2-in neçənci qüvvəti (və ya neçənci qüvvətinə yaxın) olduğunu tapın
+//int a = 5;
+//int powerOfNum = 1;
+//int number = 0;
+//int powDiff = 0;
+//int powCur = 0;
+//while (powerOfNum < a)
+//{
+//    powDiff = powCur;
+//    powerOfNum *= 2;
+//    number++;
+//    powCur = a - powerOfNum;
+//}
+//if (powCur < 0)
+//{
+//    powCur *= -1;
+//}
+//if (powDiff < powCur)
+//{
+//    number--;
+//}
+//Console.WriteLine(number);
+#endregion
+
+
+
+
+
+
+
+
+#region Task 2. Ədədin rəqəmləri cəmini tapın\
+//int a = 573;
+//int sum = 0;
+//while (a > 0)
+//{
+//	sum = sum + a % 10;
+//	a = a / 10;
+//}
+//Console.WriteLine(sum);
+#endregion
+
+
+
+
+#region Task 3. Daxil olunan müsbət tam ədədin sadə və ya mürəkkəb olduğunu tapın
+//int number = 17;
+//for (int i = 2; i < number; i++)
+//{
+//	if (number % i == 0)
+//	{
+//		Console.WriteLine("Murekkeb");
+//		break;
+//	}
+//}
+//Console.WriteLine("Sade");
+#endregion
